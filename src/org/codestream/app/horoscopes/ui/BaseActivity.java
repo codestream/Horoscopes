87508fb575c1a2fe0b89f535b6cbb3e640ea3864
Viacheslav Okolitiy <justforgit@gmail.com>
2012-09-23 01:58:33 +0300

    Added base activity. This activity will be as base class for all activities

--- src/org/codestream/app/horoscopes/ui/BaseActivity.java
+++ src/org/codestream/app/horoscopes/ui/BaseActivity.java
@@ -1,12 +1,21 @@
 package org.codestream.app.horoscopes.ui;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.Toast;
+import org.codestream.app.horoscopes.R;
 
 public abstract class BaseActivity extends Activity {
 
@@ -28,4 +37,34 @@ public abstract class BaseActivity extends Activity {
     public boolean setupMenuOptionItemSelected(MenuItem item){
         return true;
     }
+
+    public void onOpenAboutDialog() throws PackageManager.NameNotFoundException {
+        Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.layout_about);
+        PackageInfo packageInfo = getPackageManager().getPackageInfo(getPackageName(),0);
+        String stringVersion = packageInfo.versionName;
+        dialog.setTitle(getResources().getString(R.string.app_name) + " v" + stringVersion);
+        Button button = (Button)dialog.findViewById(R.id.feedbackButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent emailIntent = new Intent(Intent.ACTION_SEND);
+                emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[]{"okolitiy.viacheslav@gmail.com"});
+                emailIntent.setType("message/rfc822");
+                startActivity(Intent.createChooser(emailIntent, "Select email application."));
+            }
+        });
+
+        dialog.show();
+    }
+
+    public void showParsingError(){
+        Toast toast = Toast.makeText(getApplicationContext(),"Error while fetching data",500);
+        toast.setGravity(Gravity.CENTER,19,19);
+        toast.show();
+    }
+
+    protected abstract void cacheCurrentHoroscope();
+
+    protected abstract void saveCurrentHoroscope();
 }
