d6de5ea725fb30ed5dedd6f4964b8302bf1a9798
Viacheslav Okolitiy <justforgit@gmail.com>
2012-09-24 01:56:44 +0300

    Some UI changes

--- src/org/codestream/app/horoscopes/ui/week/GeminiWeekActivity.java
+++ src/org/codestream/app/horoscopes/ui/week/GeminiWeekActivity.java
@@ -1,6 +1,7 @@
 package org.codestream.app.horoscopes.ui.week;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
@@ -17,17 +18,19 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import org.codestream.app.horoscopes.provider.HoroscopeDatabase;
+import org.codestream.app.horoscopes.ui.BaseActivity;
 import org.codestream.app.horoscopes.utils.HoroscopeClipboard;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 import org.codestream.app.horoscopes.R;
 
-public class GeminiWeekActivity extends Activity implements HoroscopeClipboard {
+public class GeminiWeekActivity extends BaseActivity implements HoroscopeClipboard {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_gemini_week);
-        new AsyncGeminiWeekHoroscope().execute();
+        AsyncTask<Void,Integer,String> asyncTask = new AsyncGeminiWeekHoroscope(this);
+        asyncTask.execute();
     }
 
     @Override
@@ -51,7 +54,13 @@ public class GeminiWeekActivity extends Activity implements HoroscopeClipboard {
         }
     }
 
-    private void saveCurrentHoroscope(){
+    @Override
+    protected void cacheCurrentHoroscope() {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    protected void saveCurrentHoroscope(){
         HoroscopeDatabase horoscopeDatabase = new HoroscopeDatabase(GeminiWeekActivity.this);
         SQLiteDatabase sqLiteDatabase = horoscopeDatabase.getWritableDatabase();
         TextView textView = (TextView)findViewById(R.id.tvGeminiWeek);
@@ -74,6 +83,22 @@ public class GeminiWeekActivity extends Activity implements HoroscopeClipboard {
     }
 
     private class AsyncGeminiWeekHoroscope extends AsyncTask<Void,Integer,String> {
+        private Context mContext;
+        private ProgressDialog mDialog;
+
+        public AsyncGeminiWeekHoroscope(Context context){
+            this.mContext = context;
+            this.mDialog = new ProgressDialog(mContext);
+        }
+
+        @Override
+        protected void onPreExecute() {
+            mDialog.setTitle("Loading horoscopes");
+            mDialog.setMessage("Please wait....");
+            mDialog.setIndeterminate(false);
+            mDialog.setCancelable(false);
+            mDialog.show();
+        }
 
         private static final String TAG = "AsyncGeminiHoroscope";
         @Override
@@ -98,6 +123,9 @@ public class GeminiWeekActivity extends Activity implements HoroscopeClipboard {
             TextView textView = (TextView)findViewById(R.id.tvGeminiWeek);
             textView.setMovementMethod(new ScrollingMovementMethod());
             textView.setText(result);
+            if(mDialog.isShowing()){
+                mDialog.dismiss();
+            }
         }
     }
 }
